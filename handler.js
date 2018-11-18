4ce2900f727445592fb97ba6d647463a141ac108
d6er <d6er@qq.com>
2018-11-18 23:37:25 +0900

    Add template file

--- handler.js
+++ handler.js
@@ -7,12 +7,18 @@ const app = new Vue({
 })
 
 // Step 2: Create a renderer
-const renderer = require('vue-server-renderer').createRenderer()
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./index.template.html', 'utf-8')
+})
 
 module.exports.hello = async (event, context, callback) => {
   
+  const ssrContext = {
+    title: 'Vue SSR Serverless'
+  }
+  
   // Step 3: Render the Vue instance to HTML
-  renderer.renderToString(app, (err, html) => {
+  renderer.renderToString(app, ssrContext, (err, html) => {
     if (err) throw err
     
     const response = {
