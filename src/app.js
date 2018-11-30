924b1805d1060d659adc32d4433c3cd7b5c53e82
Yoshihiro Watanabe <ywatanabe@digitalriver.com>
2018-11-30 17:58:32 +0900

    amplify

--- src/app.js
+++ src/app.js
@@ -4,6 +4,39 @@ import { createRouter } from './router'
 import { createStore } from './store'
 import { sync } from 'vuex-router-sync'
 
+import Amplify, { Auth } from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
+
+const oauth = {
+  domain : 'sls-d6er-com.auth.us-east-1.amazoncognito.com', 
+  scope : ['phone', 'email', 'profile', 'openid','aws.cognito.signin.user.admin'], 
+  redirectSignIn : 'https://sls.d6er.com/signin', 
+  redirectSignOut : 'https://sls.d6er.com/signout',
+  responseType: 'code',
+  options: {
+    AdvancedSecurityDataCollectionFlag : true
+  }
+}
+
+Amplify.configure({
+  Auth: {
+    oauth: oauth
+  }
+});
+
+const config = Auth.configure();
+const { 
+  domain,  
+  redirectSignIn, 
+  redirectSignOut,
+  responseType } = config.oauth;
+
+const clientId = config.userPoolWebClientId;
+const url = 'https://' + domain + '/login?redirect_uri=' + redirectSignIn + '&response_type=code' + responseType + '&client_id=' + clientId;
+window.location.assign(url);
+
+
 export function createApp () {
   
   const router = createRouter()
