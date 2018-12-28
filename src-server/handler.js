c6dc52e7f637c4f9ed63ca1d0ea21f606fc7afd5
Yoshihiro Watanabe <ywatanabe@digitalriver.com>
2018-12-28 18:01:52 +0900

    merge handler

--- src-server/handler.js
+++ src-server/handler.js
@@ -2,62 +2,43 @@
 
 require = require("esm")(module/*, options*/)
 
-const fs = require('fs')
 const mongo = require('./mongo')
 const util = require('util')
-const api2 = require('./api2').default
-
-const { createBundleRenderer } = require('vue-server-renderer')
-
-const template = fs.readFileSync('./src/index.template.html', 'utf-8')
-const serverBundle = require('../dist/vue-ssr-server-bundle.json')
-const clientManifest = require('../dist/vue-ssr-client-manifest.json')
-
+const ssr = require('./ssr')
+const api = require('./api').default
 const cookie = require('cookie')
 
-const renderer = createBundleRenderer(serverBundle, {
-  runInNewContext: false,
-  template,
-  clientManifest
-})
+let coldStart = true
 
 module.exports.index = async (event, context) => {
+  
+  console.log('[handler.js] ' + event.path + ' coldStart:' + coldStart)
+  coldStart = false
   
   context.callbackWaitsForEmptyEventLoop = false
   
   const cookies = event.headers.hasOwnProperty('Cookie') ? cookie.parse(event.headers.Cookie) : ''
   
-  const appContext = {
-    url: event.path,
-    cookies: cookies,
-    title: event.headers.Host
+  if (event.path == '/api') {
+    
+    const payload = JSON.parse(event.body)
+    const result = await api(cookies, payload)
+    
+    const response = {
+      statusCode: 200,
+      headers: { "Content-Type": "text/html" },
+      body: JSON.stringify(result)
+    }
+    
+    return response
   }
   
-  const html = await util.promisify(renderer.renderToString)(appContext)
+  const html = await util.promisify(ssr)(event, cookies)
   
   const response = {
     statusCode: 200,
     headers: { "Content-Type": "text/html" },
     body: html
   }
-  
-  return response
-}
-
-module.exports.api = async (event, context) => {
-  
-  context.callbackWaitsForEmptyEventLoop = false;
-  
-  const cookies = event.headers.hasOwnProperty('Cookie') ? cookie.parse(event.headers.Cookie) : ''
-  const payload = JSON.parse(event.body)
-  
-  const result = await api2(cookies, payload)
-
-  const response = {
-    statusCode: 200,
-    headers: { "Content-Type": "text/html" },
-    body: JSON.stringify(result)
-  }
-  
   return response
 }
